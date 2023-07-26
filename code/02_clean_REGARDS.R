344130dc2c61507b8dd89dc7b67147f4b41e5442
samjaros-stanford <76895253+samjaros-stanford@users.noreply.github.com>
2023-07-25 23:04:25 -0700

    Correct geography selecting

--- code/02_clean_REGARDS.R
+++ code/02_clean_REGARDS.R
@@ -18,7 +18,7 @@ regards_analysis = raw_regards %>%
   # Format geography data
   mutate(geo_zcta5 = if_else(grepl("[0-9]{5}", GEO_Zip, perl=T), GEO_Zip, NA_character_),
          geo_county5 = str_c(str_pad(GEO_StateFIPS,2,"left","0"), str_pad(GEO_CountyFIPS,3,"left","0")), 
-         tract_FIPS11 = str_c(geo_county5, str_pad(GEO_Tract,6,"left","0"))) %>%
+         geo_tract_11 = str_c(geo_county5, str_pad(GEO_Tract,6,"left","0"))) %>%
   # Rename & select variables needed for analysis
   # No Cystatain C in REGARDS right now
   mutate(id=paste0("R", id),
@@ -39,7 +39,7 @@ regards_analysis = raw_regards %>%
          #cyst_c=cysc, 
          crp=Crp) %>%
   select(id, study, sys_bp, dia_bp, age, gender, race, educ, alcohol, smoke, bmi, 
-         hdl, ldl, diabetes, cyst_c, crp, starts_with("geo_"))
+         hdl, ldl, diabetes, cyst_c, crp, starts_with("geo_", ignore.case=F))
 
 # Get missingness
 print(regards_analysis %>%
