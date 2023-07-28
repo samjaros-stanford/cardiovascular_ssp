40fcd587c2544f388ac36aa9a84eaede1cee66b3
samjaros-stanford <76895253+samjaros-stanford@users.noreply.github.com>
2023-07-28 00:25:28 -0700

    Use readr read_csv to correct stop changing strings to numeric

--- code/03_get_ICE.R
+++ code/03_get_ICE.R
@@ -4,31 +4,31 @@ library(tidyverse)
 # Import #
 ##########
 # ACS Data for REGARDS
-ice_county_acs = read.csv("https://raw.githubusercontent.com/samjaros-stanford/spatial_social_polarization_database/main/ICE/ice_acs_2010_county.csv") %>%
+ice_county_acs = read_csv("https://raw.githubusercontent.com/samjaros-stanford/spatial_social_polarization_database/main/ICE/ice_acs_2010_county.csv", show_col_types=F) %>%
   select(-ICEedu) %>%
-  left_join(read.csv("https://raw.githubusercontent.com/samjaros-stanford/spatial_social_polarization_database/main/ICE/ice_acs_2012_county.csv") %>%
+  left_join(read_csv("https://raw.githubusercontent.com/samjaros-stanford/spatial_social_polarization_database/main/ICE/ice_acs_2012_county.csv", show_col_types=F) %>%
               select(GEOID, ICEedu),
             by="GEOID") %>%
   rename_with(~paste0(.x,"_county"), -GEOID)
-ice_zcta_acs = read.csv("https://raw.githubusercontent.com/samjaros-stanford/spatial_social_polarization_database/main/ICE/ice_acs_2011_zcta.csv") %>%
+ice_zcta_acs = read_csv("https://raw.githubusercontent.com/samjaros-stanford/spatial_social_polarization_database/main/ICE/ice_acs_2011_zcta.csv", show_col_types=F) %>%
   select(-ICEedu) %>%
-  left_join(read.csv("https://raw.githubusercontent.com/samjaros-stanford/spatial_social_polarization_database/main/ICE/ice_acs_2012_zcta.csv") %>%
+  left_join(read_csv("https://raw.githubusercontent.com/samjaros-stanford/spatial_social_polarization_database/main/ICE/ice_acs_2012_zcta.csv", show_col_types=F) %>%
               select(GEOID, ICEedu),
             by="GEOID") %>%
   rename_with(~paste0(.x,"_zcta"), -GEOID)
-ice_tract_acs = read.csv("https://raw.githubusercontent.com/samjaros-stanford/spatial_social_polarization_database/main/ICE/ice_acs_2010_tract.csv") %>%
+ice_tract_acs = read_csv("https://raw.githubusercontent.com/samjaros-stanford/spatial_social_polarization_database/main/ICE/ice_acs_2010_tract.csv", show_col_types=F) %>%
   select(-ICEedu) %>%
-  left_join(read.csv("https://raw.githubusercontent.com/samjaros-stanford/spatial_social_polarization_database/main/ICE/ice_acs_2012_tract.csv") %>%
+  left_join(read_csv("https://raw.githubusercontent.com/samjaros-stanford/spatial_social_polarization_database/main/ICE/ice_acs_2012_tract.csv", show_col_types=F) %>%
               select(GEOID, ICEedu),
             by="GEOID") %>%
   rename_with(~paste0(.x,"_tract"), -GEOID)
 
 # Decennial census data for CHS
-ice_county_dec = read.csv("https://raw.githubusercontent.com/samjaros-stanford/spatial_social_polarization_database/main/ICE/ice_dec_2000_county.csv") %>%
+ice_county_dec = read_csv("https://raw.githubusercontent.com/samjaros-stanford/spatial_social_polarization_database/main/ICE/ice_dec_2000_county.csv", show_col_types=F) %>%
   rename_with(~paste0(.x,"_county"),-GEOID)
-ice_zcta_dec = read.csv("https://raw.githubusercontent.com/samjaros-stanford/spatial_social_polarization_database/main/ICE/ice_dec_2000_zcta.csv") %>%
+ice_zcta_dec = read_csv("https://raw.githubusercontent.com/samjaros-stanford/spatial_social_polarization_database/main/ICE/ice_dec_2000_zcta.csv", show_col_types=F) %>%
   rename_with(~paste0(.x,"_zcta"),-GEOID)
-ice_tract_dec = read.csv("https://raw.githubusercontent.com/samjaros-stanford/spatial_social_polarization_database/main/ICE/ice_dec_2000_tract.csv") %>%
+ice_tract_dec = read_csv("https://raw.githubusercontent.com/samjaros-stanford/spatial_social_polarization_database/main/ICE/ice_dec_2000_tract.csv", show_col_types=F) %>%
   rename_with(~paste0(.x,"_tract"),-GEOID)
 
 ##########
