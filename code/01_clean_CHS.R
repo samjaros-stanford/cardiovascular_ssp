c7766cfc073de52c091e6eb3db026b48273d3edf
samjaros-stanford <76895253+samjaros-stanford@users.noreply.github.com>
2023-07-27 22:50:26 -0700

    Add variable harmonization to cleaning step

--- code/01_clean_CHS.R
+++ code/01_clean_CHS.R
@@ -34,26 +34,70 @@ chs_analysis = raw_chs %>%
               select(idno, z10_cen_uid_u_2010) %>%
               rename(geo_zcta5=z10_cen_uid_u_2010),
             by="idno") %>%
-  # Rename & select variables needed for analysis
-  mutate(idno = paste0("C", idno),
-         study = "CHS") %>%
-  rename(id=idno,
-         sys_bp=avesys2, 
-         dia_bp=avedia2, 
-         age=agebl, 
-         gender=gend01, 
-         race=race01, 
-         educ=grade01, 
-         alcohol=alcoh2, 
-         smoke=smoke2, 
-         bmi=bmi2, 
-         hdl=hdl2, 
-         ldl=ldladj2, 
-         diabetes=diabada2, 
-         cyst_c=cystatc2, 
-         crp=crp2) %>%
+  # Harmonize variables needed for analysis
+  #   Add a C to the patient ID for CHS and add a study identifier column
+  #   CHS was recruited in 2 waves - The baseline for wave 1 is year 2, wave 2 is year 5
+  mutate(id = paste0("C", idno),
+         study = "CHS",
+         age = agebl,
+         # Harmonize demographics to match REGARDS
+         #   Gender = Male/Female
+         gend01 = str_trim(gend01),
+         gender = case_when(
+           gend01=="male"   ~ "Male",
+           gend01=="female" ~ "Female",
+           T                ~ NA_character_),
+         #   Education = Less than HS, High School, Some College, College and above
+         grade01 = str_trim(grade01),
+         educ = case_when(
+           grade01 == "no schooling"             ~ "Less than HS",
+           grade01 == "grade 12"                 ~ "High school",
+           grade01 == "ged"                      ~ "High school",
+           grade01 == "4 years college"          ~ "College and above",
+           grade01 == "graduate or professional" ~ "College and above",
+           grepl("grade", grade01)               ~ "Less than HS",
+           grepl("college", grade01)             ~ "Some college",
+           grepl("vocational", grade01)          ~ "High school",
+           T                                     ~ NA_character_),
+         #   Race = Odden collapses to Black vs White/Other due to most patients being White or Black
+         race = case_when(
+           race01 == "black"                     ~ "Black",
+           grepl("^(?!\\s*$).+", race01, perl=T) ~ "White/Other",
+           T                                     ~ NA_character_),
+         #   Smoke = 1-Never, 2-Former, 3-Current
+         smoke = if_else(perstat=="old cohort", smoke2, smoke5),
+         smoke = case_when(
+           smoke == 1 ~ "Never",
+           smoke == 2 ~ "Former",
+           smoke == 3 ~ "Current",
+           T          ~ NA_character_),
+         #   Diabetes = No - normal & ifg, Yes - known diabetes & new diabetes
+         diabetes = if_else(perstat=="old cohort", diabada2, diabada5),
+         diabetes = case_when(
+           grepl("diabetes", diabetes) ~ "Diabetes",
+           diabetes == "normal"        ~ "Normal",
+           diabetes == "ifg"           ~ "Normal",
+           T                           ~ NA_character_),
+         # Select proper timepoint based on wave
+         sys_bp   =if_else(perstat=="old cohort",avesys2,  avesys5), 
+         dia_bp   =if_else(perstat=="old cohort",avedia2,  avedia5), 
+         alcohol  =if_else(perstat=="old cohort",alcoh2,   alcoh5),
+         bmi      =if_else(perstat=="old cohort",bmi2,     bmi5),
+         hdl      =if_else(perstat=="old cohort",hdl2,     hdl5),
+         ldl      =if_else(perstat=="old cohort",ldladj2,  ldladj5),
+         crp      =if_else(perstat=="old cohort",crp2,     crp5)) %>%
+  # Calculate CKD-EPI as described by Levey et al. 2009
+  mutate(scr   = if_else(perstat=="old cohort", cre2clb, cre5clb),
+         kappa = if_else(gender=="female",      0.7,     0.9),
+         alpha = if_else(gender=="female",      -0.329,  -0.411),
+         final = if_else(gender=="female",      1.018,   1)*
+                 if_else(race=="black",         1.159,   1)) %>%
+  rowwise() %>%
+  mutate(egfr_ckdepi = 141*min(scr/kappa,1)^alpha*max(scr/kappa,1)^-1.209*0.993^age*final) %>%
+  ungroup() %>%
+  # Keep only variables of interest
   select(id, study, sys_bp, dia_bp, age, gender, race, educ, alcohol, smoke, bmi, 
-         hdl, ldl, diabetes, cyst_c, crp, starts_with("geo_"))
+         hdl, ldl, diabetes, egfr_ckdepi, crp, starts_with("geo_"))
 
 # Get missingness
 chs_analysis %>%
--- code/02_clean_REGARDS.R
+++ code/02_clean_REGARDS.R
@@ -18,28 +18,50 @@ regards_analysis = raw_regards %>%
   # Format geography data
   mutate(geo_zcta5 = if_else(grepl("[0-9]{5}", GEO_Zip, perl=T), GEO_Zip, NA_character_),
          geo_county5 = str_c(str_pad(GEO_StateFIPS,2,"left","0"), str_pad(GEO_CountyFIPS,3,"left","0")), 
-         geo_tract_11 = str_c(geo_county5, str_pad(GEO_Tract,6,"left","0"))) %>%
+         geo_tract11 = str_c(geo_county5, str_pad(GEO_Tract,6,"left","0"))) %>%
   # Rename & select variables needed for analysis
-  # No Cystatain C in REGARDS right now
   mutate(id=paste0("R", id),
          study="REGARDS",
-         cyst_c=NA) %>%
+         # Harmonize demographics to match CHS
+         #   Gender = Male/Female
+         gender = case_when(
+           Gender=="F" ~ "Female",
+           Gender=="M" ~ "Male",
+           T           ~ NA_character_),
+         #   Education = Less than HS, High School, Some College, College and above
+         educ = case_when(
+           ED_Cat == "Less than high school"      ~ "Less than HS",
+           ED_Cat == "High school graduate"       ~ "High school",
+           ED_Cat == "Some college"               ~ "Some college",
+           ED_Cat == "College graduate and above" ~ "College and above",
+           T                                      ~ NA_character_),
+         #   Race = Black/White
+         race = case_when(
+           Race == "B" ~ "Black",
+           Race == "W" ~ "White",
+           T           ~ NA_character_),
+         #   Smoker = Never/Former/Current
+         smoke = case_when(
+           Smoke == "Never"   ~ "Never",
+           Smoke == "Past"    ~ "Former",
+           Smoke == "Current" ~ "Current",
+           T                  ~ NA_character_),
+         #   Diabetes = Diabetes/Normal
+         diabetes = case_when(
+           Diabetes_SR == "Y" ~ "Diabetes",
+           Diabetes_SR == "N" ~ "Normal",
+           T                  ~ NA_character_)) %>%
   rename(sys_bp=SBP, 
          dia_bp=DBP, 
          age=Age, 
-         gender=Gender, 
-         race=Race, 
-         educ=ED_Cat, 
          alcohol=Alc_Drinks_Wk, 
-         smoke=Smoke, 
          bmi=BMI, 
          hdl=Hdl, 
          ldl=Ldl, 
-         diabetes=Diabetes_SR, 
-         #cyst_c=cysc, 
+         egfr_ckdepi=EGFR_CKDEPI,
          crp=Crp) %>%
   select(id, study, sys_bp, dia_bp, age, gender, race, educ, alcohol, smoke, bmi, 
-         hdl, ldl, diabetes, cyst_c, crp, starts_with("geo_", ignore.case=F))
+         hdl, ldl, diabetes, egfr_ckdepi, crp, starts_with("geo_", ignore.case=F))
 
 # Get missingness
 print(regards_analysis %>%
