ff4f898b662e33c8242803fb7353cc747814961b
samjaros-stanford <76895253+samjaros-stanford@users.noreply.github.com>
2024-01-23 17:22:46 -0800

    With new unified census tracts, remove ACS join Change variable factoring to base R for speed

--- code/09_assemble_analysis.R
+++ code/09_assemble_analysis.R
@@ -1,46 +1,41 @@
 library(tidyverse)
-library(stringr)
 
 ##############
 # Processing #
 ##############
 # Assemble CHS & REGARDS into final analysis dataset
-full_analysis = rbind(
+full_analysis = 
   # Stack harmonized CHS & REGARDS
+  rbind(readRDS("data/chs_analysis.rds"), readRDS("data/regards_analysis.rds")) %>%
+  ungroup() %>%
   # Merge in proper census (2000 decennial for CHS & combined 2010-12 ACS for REGARDS)
-  readRDS("data/chs_analysis.rds") %>% 
-    left_join(readRDS("data/ICE/ice_county_acs.rds"), by=c("geo_county5"="GEOID")) %>%
-    left_join(readRDS("data/ICE/ice_zcta_acs.rds"), by=c("geo_zcta5"="GEOID")) %>%
-    left_join(readRDS("data/ICE/ice_tract_acs.rds"), by=c("geo_tract11"="GEOID")), 
-  readRDS("data/regards_analysis.rds") %>%
-    left_join(readRDS("data/ICE/ice_county_dec.rds"), by=c("geo_county5"="GEOID")) %>%
-    left_join(readRDS("data/ICE/ice_zcta_dec.rds"), by=c("geo_zcta5"="GEOID")) %>%
-    left_join(readRDS("data/ICE/ice_tract_dec.rds"), by=c("geo_tract11"="GEOID"))) %>%
-  # Factor variables as needed
-  mutate(id = factor(id),
-         study = factor(study, levels=c("CHS", "REGARDS")),
-         ### Demographics
-         gender = factor(gender, levels=c("Male", "Female")),
-         race = factor(race, levels=c("White/Other", "Black")),
-         educ = factor(educ, levels=c("College and above", "Some college", "High school", "Less than HS")),
-         ### Medical history
-         diabetes = factor(diabetes, levels=c("Normal", "Diabetes")),
-         stroke = factor(stroke, levels=c("No Stroke", "Stroke")),
-         mi = factor(mi, levels=c("No MI", "MI")),
-         ### Comorbidities
-         smoke = factor(smoke, levels=c("Never", "Former", "Current"))) %>%
+  left_join(readRDS("data/ICE/ice_county_dec.rds"), by=c("geo_county5"="GEOID")) %>%
+  left_join(readRDS("data/ICE/ice_zcta_dec.rds"), by=c("geo_zcta5"="GEOID")) %>%
+  left_join(readRDS("data/ICE/ice_tract_dec.rds"), by=c("geo_tract11"="GEOID")) %>%
   # Construct primary outcomes - Hypertension 1 & 2
   mutate(hbp1 = sys_bp>=140 | dia_bp >=90,
          hbp2 = sys_bp>=130 | dia_bp >=80) %>%
   # Get ICE tertile & quantile values
   mutate(across(starts_with("ICE"), 
-                .fns=list(tert=~factor(ntile(.x, 3),levels=c(3:1)), quant=~factor(ntile(.x, 5),levels=c(5:1))),
-                .names="{.col}_{.fn}")) %>%
+                .fns=~factor(ntile(.x, 5),levels=c(5:1)),
+                .names="{.col}_quint")) %>%
   rename_with(~str_replace_all(.x, "_raw_", "_"), starts_with("ICE"))
+  
+# Factor variables as needed
+full_analysis$id = factor(full_analysis$id)
+full_analysis$study = factor(full_analysis$study, levels=c("CHS", "REGARDS"))
+full_analysis$gender = factor(full_analysis$gender, levels=c("Male", "Female"))
+full_analysis$race = factor(full_analysis$race, levels=c("White/Other", "Black"))
+full_analysis$educ = factor(full_analysis$educ, levels=c("College and above", "Some college", "High school", "Less than HS"))
+full_analysis$diabetes = factor(full_analysis$diabetes, levels=c("Normal", "Diabetes"))
+full_analysis$stroke = factor(full_analysis$stroke, levels=c("No Stroke", "Stroke"))
+full_analysis$mi = factor(full_analysis$mi, levels=c("No MI", "MI"))
+full_analysis$smoke = factor(full_analysis$smoke, levels=c("Never", "Former", "Current"))
 
 # Get missingness
 print(full_analysis %>%
         #filter(study=="CHS") %>%
+        ungroup() %>%
         summarize(across(everything(), ~mean(is.na(.x))*100)) %>%
         pivot_longer(cols = everything(),
                      names_to = "col",
